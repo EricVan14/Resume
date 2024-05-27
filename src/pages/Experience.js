8b9312ad677b64e5209f57f3b02efe249fa98baf
EricVan14 <105650647+EricVan14@users.noreply.github.com>
2024-05-27 15:13:47 -0400

    fixed lists

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -5,39 +5,55 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import ParallaxSection from '../components/ParallaxSection';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 import image from '../assets/pics/arsenalAVIF.avif';
 
 const Experience = () => {
   return (
     <>
       <ParallaxSection image={image}>
-        <Typography variant="h2" component="h1">
+        <Typography variant="h2" component="h1" align="center" gutterBottom>
           Experience
         </Typography>
         <Container>
-            <Box sx={{ my: 4 }}>
+          <Box sx={{ my: 4 }}>
             <Typography variant="h6" component="p" gutterBottom>
-                SaskPower – Regina, Saskatchewan (in office), 2023
+              SaskPower – Regina, Saskatchewan (in office), 2023
             </Typography>
-            <ul>
-                <li>Analyzed, designed, and developed applications using the latest technologies.</li>
-                <li>Application UI design, JSON data modelling, bug fixing, and problem solving.</li>
-                <li>Collaborative development using Git and Azure DevOps.</li>
-                <li>Gathered functional requirements from external and/or internal customers and end-users.</li>
-            </ul>
+            <List>
+              <ListItem>
+                <ListItemText primary="Analyzed, designed, and developed applications using the latest technologies." />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Application UI design, JSON data modelling, bug fixing, and problem solving." />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Collaborative development using Git and Azure DevOps." />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Gathered functional requirements from external and/or internal customers and end-users." />
+              </ListItem>
+            </List>
             <Divider sx={{ my: 2 }} />
             <Typography variant="h6" component="p" gutterBottom>
-                Metal Pros - Ottawa, Ontario, 2021-2022
+              Metal Pros - Ottawa, Ontario, 2021-2022
             </Typography>
-            <ul>
-                <li>Prepared and organized shipments and orders in a fast-paced, ever-changing environment.</li>
-                <li>Demonstrated advanced problem-solving, teamwork, and efficiency skills.</li>
-                <li>Planned organization strategies for a new facility.</li>
-            </ul>
-            </Box>
+            <List>
+              <ListItem>
+                <ListItemText primary="Prepared and organized shipments and orders in a fast-paced, ever-changing environment." />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Demonstrated advanced problem-solving, teamwork, and efficiency skills." />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Planned organization strategies for a new facility." />
+              </ListItem>
+            </List>
+          </Box>
         </Container>
       </ParallaxSection>
-      
     </>
   );
 };
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,54 +5,70 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import ParallaxSection from '../components/ParallaxSection';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 import image from '../assets/pics/arsenalAVIF.avif';
 
 const Projects = () => {
   return (
     <>
       <ParallaxSection image={image}>
-        <Typography variant="h2" component="h1">
+        <Typography variant="h2" component="h1" align="center" gutterBottom>
           Projects
         </Typography>
         <Container>
-            <Box sx={{ my: 4 }}>
+          <Box sx={{ my: 4 }}>
             <Typography variant="h6" component="p" gutterBottom>
-                Honours Project (with Prof M. Garzon)
+              Honours Project (with Prof M. Garzon)
             </Typography>
             <Typography variant="body1" component="p" gutterBottom>
-                Detection of Tor/VPN/Proxy traffic in Web access logs using ML, 2024
+              Detection of Tor/VPN/Proxy traffic in Web access logs using ML, 2024
             </Typography>
-            <ul>
-                <li>Created algorithms for feature extraction/selection.</li>
-                <li>Created supervised machine learning model using python-scikit learn and Random Forest classifier.</li>
-                <li>Detects Tor traffic within PCAP files at 97% accuracy.</li>
-            </ul>
+            <List>
+              <ListItem>
+                <ListItemText primary="Created algorithms for feature extraction/selection." />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Created supervised machine learning model using python-scikit learn and Random Forest classifier." />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Detects Tor traffic within PCAP files at 97% accuracy." />
+              </ListItem>
+            </List>
             <Divider sx={{ my: 2 }} />
             <Typography variant="h6" component="p" gutterBottom>
-                Course Booking App
+              Course Booking App
             </Typography>
             <Typography variant="body1" component="p" gutterBottom>
-                Created using Android Studio, 2022
+              Created using Android Studio, 2022
             </Typography>
-            <ul>
-                <li>Allows different user types to login and enroll in/assign themselves as a student or professor to different university classes (using SQL).</li>
-                <li>Allows planning out schedules.</li>
-            </ul>
+            <List>
+              <ListItem>
+                <ListItemText primary="Allows different user types to login and enroll in/assign themselves as a student or professor to different university classes (using SQL)." />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Allows planning out schedules." />
+              </ListItem>
+            </List>
             <Divider sx={{ my: 2 }} />
             <Typography variant="h6" component="p" gutterBottom>
-                FUT Draft App
+              FUT Draft App
             </Typography>
             <Typography variant="body1" component="p" gutterBottom>
-                Personal project, 2022
+              Personal project, 2022
             </Typography>
-            <ul>
-                <li>Game made in Android Studio and based off the video game FIFA.</li>
-                <li>Allows players to select a team of real soccer players complete with stats, ratings, and chemistry points.</li>
-            </ul>
-            </Box>
+            <List>
+              <ListItem>
+                <ListItemText primary="Game made in Android Studio and based off the video game FIFA." />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Allows players to select a team of real soccer players complete with stats, ratings, and chemistry points." />
+              </ListItem>
+            </List>
+          </Box>
         </Container>
       </ParallaxSection>
-      
     </>
   );
 };
